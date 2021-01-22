913e26cd3e81e4f4d7b6ec76cbb2e9b81bb4df6c
Hiba Tamimi <40838567+hibtmimi@users.noreply.github.com>
2021-01-22 22:01:36 +0200

    sidebar done

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home  from '../src/Components/Home/Home'
 import Sidebar from './Components/Sidebar/Sidebar'
+import Feed from './Components/Feed/Feed';
 
 
 function App() {
   return (
     <div className="app">
       <Sidebar/>
+      <Feed/>
      
       {/* <Switch>
           <Route exact path='/'  component={Home} />
