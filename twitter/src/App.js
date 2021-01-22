0195f9a52b9625a4ed1867993dbc292c4c2ab070
Hiba Tamimi <40838567+hibtmimi@users.noreply.github.com>
2021-01-22 21:30:33 +0200

    sidebar

--- twitter/src/App.js
+++ twitter/src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Home  from '../src/Components/Home/Home'
+import Sidebar from './Components/Sidebar/Sidebar'
 
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
+    <div className="app">
+      <Sidebar/>
+     
+      {/* <Switch>
           <Route exact path='/'  component={Home} />
-        </Switch>
+        </Switch> */}
     </div>
   );
 }
