1da741a3041ff241c433f7b1913be12e6cba6b2f
Hiba Tamimi <40838567+hibtmimi@users.noreply.github.com>
2021-01-26 13:39:48 +0200

    edit for home and profile

--- twitter/src/Components/Feed/Post.js
+++ twitter/src/Components/Feed/Post.js
@@ -11,29 +11,29 @@ function Post ({ displayName, username, verified, text, image, avatar }, ref){
     return (
       <div className="post" >
         <div className="post__avatar">
-          <Avatar src={avatar} />
-          {/* <Avatar src='https://pbs.twimg.com/profile_images/1242623772437426176/C0WdBcXb_400x400.jpg' /> */}
+          {/* <Avatar src={avatar} /> */}
+          <Avatar src='https://pbs.twimg.com/profile_images/1242623772437426176/C0WdBcXb_400x400.jpg' />
         </div>
         <div className="post__body">
           <div className="post__header">
             <div className="post__headerText">
               <h3>
-                {displayName}{" "}
-                {/* Hiba Tamimi */}
+                {/* {displayName}{" "} */}
+                Hiba Tamimi
                 <span className="post__headerSpecial">
-                  {verified && <VerifiedUserIcon className="post__badge" />} @
-                  {username}
-                  {/* hibatamimi */}
+                   <VerifiedUserIcon className="post__badge" /> @
+                  {/* {username} */}
+                  hibatamimi
                 </span>
               </h3>
             </div>
             <div className="post__headerDescription">
-              <p>{text}</p>
-              {/* lets start the challange  */}
+              {/* <p>{text}</p> */}
+              lets start the challange 
             </div>
           </div>
-          <img src={image} alt="" />
-            {/* <img src=' https://steamuserimages-a.akamaihd.net/ugc/849346468950683465/3647B7FB74DF0AB0A30B3AD17BECA2D30E75F7A2/' alt="" /> */}
+          {/* <img src={image} alt="" /> */}
+            <img src=' https://steamuserimages-a.akamaihd.net/ugc/849346468950683465/3647B7FB74DF0AB0A30B3AD17BECA2D30E75F7A2/' alt="" />
          
           <div className="post__footer">
             <ChatBubbleOutlineIcon fontSize="small" />
--- twitter/src/Components/Signup/Signup.jsx
+++ twitter/src/Components/Signup/Signup.jsx
@@ -1,130 +1,130 @@
-import { Button, TextField } from "@material-ui/core";
-import { signup } from "../../redux/actions";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import React from "react";
-import Date from "./Date";
-import "./Signup.css";
+// import { Button, TextField } from "@material-ui/core";
+// import { signup } from "../../redux/actions";
+// import { Link } from "react-router-dom";
+// import { connect } from "react-redux";
+// import React from "react";
+// import Date from "./Date";
+// import "./Signup.css";
 
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: "",
-    };
-  }
+// class Signup extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       error: "",
+//     };
+//   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+//   handleChange = (e) => {
+//     this.setState({ [e.target.name]: e.target.value });
+//   };
 
-  checkemail = (e) => {
-    this.validateEmail(e.target.value)
-      ? fetch("http://127.0.0.1:5000/email", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-          body: e.target.value,
-        })
-          .then((response) => response.json())
-          .then((result) => {
-            result === "no"
-              ? this.setState({ error: "Email has already been taken." })
-              : this.setState({ error: "" });
-          })
-          .catch((error) => console.error(error))
-      : this.setState({ error: "Please enter a valid email." });
-  };
+//   checkemail = (e) => {
+//     this.validateEmail(e.target.value)
+//       ? fetch("http://127.0.0.1:5000/email", {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//             "Access-Control-Allow-Origin": "*",
+//           },
+//           body: e.target.value,
+//         })
+//           .then((response) => response.json())
+//           .then((result) => {
+//             result === "no"
+//               ? this.setState({ error: "Email has already been taken." })
+//               : this.setState({ error: "" });
+//           })
+//           .catch((error) => console.error(error))
+//       : this.setState({ error: "Please enter a valid email." });
+//   };
 
-  validateEmail = (email) => {
-    var regex = /\S+@\S+\.\S+/;
-    return regex.test(email);
-  };
+//   validateEmail = (email) => {
+//     var regex = /\S+@\S+\.\S+/;
+//     return regex.test(email);
+//   };
 
-  render() {
-    return (
-      <div className="container_image">
-        <div className="container_signup">
-          <div className="div_button">
-            <img
-              src="https://www.lter-europe.net/document-archive/image-gallery/albums/logos/TwitterLogo_55acee.png/image"
-              alt="Bird"
-              width="60px"
-              height="60px"
-            />
-            <Link to="/form2" style={{ textDecoration: "none" }}>
-              <Button
-                type="submit"
-                className="button"
-                onClick={this.props.signup([
-                  this.state.username,
-                  this.state.email,
-                ])}
-              >
-                {" "}
-                Next
-              </Button>
-            </Link>
-          </div>
-          <div className="div_form">
-            <h2>Create your account</h2>
-            <form className="signup">
-              <TextField
-                className="Input"
-                label="username"
-                type="text"
-                name="username"
-                onChange={(e) => {
-                  this.handleChange(e);
-                }}
-                variant="outlined"
-                required
-              />
-              <br />
-              <br />
-              <TextField
-                className="Input"
-                label="Email"
-                type="email"
-                name="email"
-                onChange={(e) => {
-                  this.checkemail(e);
-                  this.handleChange(e);
-                }}
-                variant="outlined"
-                error={!!this.state.error}
-                helperText={this.state.error}
-                required
-              />
-              <h5>Use phone instead</h5>
-              <h4>Date of Birth</h4>
-              <p>
-                This will not be shown publicly. Confirm your own age, even if
-                this account is for a business, a pet, or something else.
-              </p>
-              <Date />
-            </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="container_image">
+//         <div className="container_signup">
+//           <div className="div_button">
+//             <img
+//               src="https://www.lter-europe.net/document-archive/image-gallery/albums/logos/TwitterLogo_55acee.png/image"
+//               alt="Bird"
+//               width="60px"
+//               height="60px"
+//             />
+//             <Link to="/form2" style={{ textDecoration: "none" }}>
+//               <Button
+//                 type="submit"
+//                 className="button"
+//                 onClick={this.props.signup([
+//                   this.state.username,
+//                   this.state.email,
+//                 ])}
+//               >
+//                 {" "}
+//                 Next
+//               </Button>
+//             </Link>
+//           </div>
+//           <div className="div_form">
+//             <h2>Create your account</h2>
+//             <form className="signup">
+//               <TextField
+//                 className="Input"
+//                 label="username"
+//                 type="text"
+//                 name="username"
+//                 onChange={(e) => {
+//                   this.handleChange(e);
+//                 }}
+//                 variant="outlined"
+//                 required
+//               />
+//               <br />
+//               <br />
+//               <TextField
+//                 className="Input"
+//                 label="Email"
+//                 type="email"
+//                 name="email"
+//                 onChange={(e) => {
+//                   this.checkemail(e);
+//                   this.handleChange(e);
+//                 }}
+//                 variant="outlined"
+//                 error={!!this.state.error}
+//                 helperText={this.state.error}
+//                 required
+//               />
+//               <h5>Use phone instead</h5>
+//               <h4>Date of Birth</h4>
+//               <p>
+//                 This will not be shown publicly. Confirm your own age, even if
+//                 this account is for a business, a pet, or something else.
+//               </p>
+//               <Date />
+//             </form>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
-// Redux
-const mapStateToProps = (state) => {
-  return {
-    signup: state.signup,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    signup: (x) => {
-      dispatch(signup(x));
-    },
-  };
-};
+// // Redux
+// const mapStateToProps = (state) => {
+//   return {
+//     signup: state.signup,
+//   };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     signup: (x) => {
+//       dispatch(signup(x));
+//     },
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signup);
+// export default connect(mapStateToProps, mapDispatchToProps)(Signup);
