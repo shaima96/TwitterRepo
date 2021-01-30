b479953a16ab88a366865eca1f1c06eb00921979
shaima96 <141062@ppu.edu.ps>
2021-01-30 12:59:57 +0200

    change

--- twitter/src/Components/Feed/Post.js
+++ twitter/src/Components/Feed/Post.js
@@ -4,11 +4,7 @@ import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import PublishIcon from "@material-ui/icons/Publish";
 
-class Post extends React.Component {
-  constructor(props) {
     super(props)
     this.state = {
       tweets: []
